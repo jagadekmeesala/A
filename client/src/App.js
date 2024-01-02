0ec1617bbda1d1d27f738253dc0fe34f1d37dc71
jagadekmeesala <jagadekmeesala006@gmail.com>
2024-01-02 22:25:11 +0530

    Pushed files

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import Main_page from './pages/Main_page';
+
+
+
+function App() {
+ return (
+   <div className="body">
+     <Main_page/>
+     </div>
+ );
+}
+
+export default App;
